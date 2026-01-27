3b9fcab72b79802909654c6c204b86ea06c2d0ed
chaymaMighri <chaymamighri942@gmail.com>
2026-01-27 19:25:54 +0100

    Initial commit: structure du invera project

---
+++ invera_backend/src/main/java/org/erp/invera/InVeraApplication.java
@@ -0,0 +1,13 @@
+package org.erp.invera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InVeraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InVeraApplication.class, args);
+    }
+
+}
