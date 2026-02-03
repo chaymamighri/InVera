548bf3632953adf3dc3f84ac54b5c1139c896d4c
chaymaMighri <chaymamighri942@gmail.com>
2026-02-03 15:03:11 +0100

    système d'authentification JWT avec rôles et protection d'endpoints

---
+++ invera_backend/src/main/java/org/erp/invera/model/RoleName.java
@@ -0,0 +1,11 @@
+package org.erp.invera.model;
+
+public enum RoleName {
+    /**
+     * Enum représentant les différents rôles disponibles dans l'ERP
+     */
+        ADMIN,
+        COMMERCIAL,
+        RESPONSABLE_ACHAT
+    }
+
