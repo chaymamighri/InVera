c6e5079be123c1b273d6e747ececbef638c9b4e8
nassibmim <nassibmim25@gmail.com>
2026-02-17 01:34:01 +0100

    resoudre les problem du backend liee a la gestion des utilisateur

---
+++ invera_backend/src/main/java/org/erp/invera/dto/ChangePasswordRequest.java
@@ -0,0 +1,30 @@
+package org.erp.invera.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class ChangePasswordRequest {
+
+    @NotBlank
+    private String oldPassword;
+
+    @NotBlank
+    private String newPassword;
+
+    // getters & setters
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
